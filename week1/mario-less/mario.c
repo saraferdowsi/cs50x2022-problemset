1fa8a9a9c9354ceff1d542381ef65419f9d2d53e
saraferdowsi <110331692+saraferdowsi@users.noreply.github.com>
2022-12-15 19:10:04 +0330

    Add files via upload

---
+++ week1/mario-less/mario.c
@@ -0,0 +1,28 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void) 
+{
+  int n;
+  do
+  {
+      n = get_int("size: ");
+  }
+  while(n < 1 || n > 8 );
+  
+  for (int i = 0; i<n; i++)
+  {
+      for (int j = 0; j<n; j++)
+      {
+          if(i+j < n-1)
+          printf(" ");
+          else
+          printf("#");
+
+      }
+      printf ("\n");
+  }
+  
+}
+
+
