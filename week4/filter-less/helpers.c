2c32bc453b00b2e4232ec9fdaa613c29fbaacd7b
saraferdowsi <110331692+saraferdowsi@users.noreply.github.com>
2022-12-15 21:17:39 +0330

    Add files via upload

---
+++ week4/filter-less/helpers.c
@@ -0,0 +1,142 @@
+#include "helpers.h"
+#include<math.h>
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i=0 ; i<height ; i++)
+    {
+        for (int j=0 ; j<width ; j++)
+        {
+        float Red = image[i][j].rgbtRed;
+        float Green = image[i][j].rgbtGreen;
+        float Blue = image[i][j].rgbtBlue;
+        int avrage= (round(Red+Blue+Green)/3);
+        image[i][j].rgbtRed=image[i][j].rgbtGreen=image[i][j].rgbtBlue=avrage;
+        }
+    }
+    return;
+}
+
+// Convert image to sepia
+void sepia(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i=0 ; i<height ; i++)
+    {
+        for (int j=0 ; j<width ; j++)
+        {
+            float originalRed = image[i][j].rgbtRed;
+            float originalGreen = image[i][j].rgbtGreen;
+            float originalBlue = image[i][j].rgbtBlue;
+            int sepiaRed = round(0.393 * originalRed + 0.769 * originalGreen + 0.189 * originalBlue);
+             if (sepiaRed > 255)
+            {
+                image[i][j].rgbtRed = 255;
+            }
+            else
+            {
+                image[i][j].rgbtRed = sepiaRed;
+            }
+            int sepiaGreen = round(0.349 * originalRed + 0.686 * originalGreen + 0.168 * originalBlue);
+            if (sepiaGreen > 255)
+            {
+                image[i][j].rgbtGreen = 255;
+            }
+            else
+            {
+                image[i][j].rgbtGreen = sepiaGreen;
+            }
+            int sepiaBlue = round(0.272 * originalRed + 0.534 * originalGreen + 0.131 * originalBlue);
+            if (sepiaBlue > 255)
+            {
+                image[i][j].rgbtBlue = 255;
+            }
+            else
+            {
+                image[i][j].rgbtBlue = sepiaBlue;
+            }
+        }
+    }
+    return;
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+     for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < (width/2); j++)
+        {
+            int temPred = image[i][j].rgbtRed;
+            int temPgreen = image[i][j].rgbtGreen;
+            int temPblue = image[i][j].rgbtBlue;
+
+            image[i][j].rgbtRed = image[i][width - j - 1].rgbtRed;
+            image[i][j].rgbtGreen = image[i][width - j - 1].rgbtGreen;
+            image[i][j].rgbtBlue = image[i][width - j - 1].rgbtBlue;
+
+            image[i][width - j - 1].rgbtRed = temPred;
+            image[i][width - j - 1].rgbtGreen = temPgreen;
+            image[i][width - j - 1].rgbtBlue = temPblue;
+        }
+    }
+
+    return;
+}
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+        RGBTRIPLE temp[height][width];
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int total_Red, total_Blue, total_Green;
+            total_Red = total_Blue = total_Green = 0;
+            float counter = 0.00;
+
+            //Get the neighbouring pexels
+            for (int x = -1; x < 2; x++)
+            {
+                for (int y = -1; y < 2; y++)
+                {
+                    int currentX = i + x;
+                    int currentY = j + y;
+
+                    //check for valid neighbouring pexels
+                    if (currentX < 0 || currentX > (height - 1) || currentY < 0 || currentY > (width - 1))
+                    {
+                        continue;
+                    }
+
+                    //Get the image value
+                    total_Red += image[currentX][currentY].rgbtRed;
+                    total_Green += image[currentX][currentY].rgbtGreen;
+                    total_Blue += image[currentX][currentY].rgbtBlue;
+
+                    counter++;
+                }
+
+                //do the average of neigbhouring pexels
+                temp[i][j].rgbtRed = round(total_Red / counter);
+                temp[i][j].rgbtGreen = round(total_Green / counter);
+                temp[i][j].rgbtBlue = round(total_Blue / counter);
+            }
+        }
+
+    }
+
+    //copy the blurr image to the original image
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j].rgbtRed = temp[i][j].rgbtRed;
+            image[i][j].rgbtGreen = temp[i][j].rgbtGreen;
+            image[i][j].rgbtBlue = temp[i][j].rgbtBlue;
+        }
+    }
+    return;
+}
